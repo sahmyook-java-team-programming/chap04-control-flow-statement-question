985028b334cddc2d557f3ff7efeb0525b6fdeb82
chodonghoon2 <112090479+chodonghoon2@users.noreply.github.com>
2023-05-05 22:55:19 +0900

    [문제1] 조건문1
    조건식이 참,거짓에 따라 결과가 나오게 if문을 통해 출력

--- src/main/java/com/ohgiraffers/section01/conditional/level01/basic/Application1.java
+++ src/main/java/com/ohgiraffers/section01/conditional/level01/basic/Application1.java
@@ -1,9 +1,11 @@
 package com.ohgiraffers.section01.conditional.level01.basic;
 
+import java.util.Scanner;
+
 public class Application1 {
 
 	public static void main(String[] args) {
-		
+
 		/* 정수를 하나 입력 받아 그 수가 양수이면 "양수다." 라고 출력하고,
 		 * 양수가 아닌 경우 "양수가 아니다." 라고 출력하세요
 		 * 
@@ -14,6 +16,17 @@ public class Application1 {
 		 * 양수다.
 		 * */
 
+		Scanner sc = new Scanner(System.in);
+		System.out.print("숫자를 입력하세요");
+		int x = sc.nextInt();
+
+		if (x > 0 ) {
+			System.out.println("양수다.");
+		} else if (x < 0) {
+			System.out.println("양수가 아니다.");
+		} else {
+			System.out.println("0 입니다.");
+		}
 	}
 
 }
